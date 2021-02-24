c6e9696dd26c997d8b01afeebb4af61977957fe4
Allie <alliecaton3@gmail.com>
2021-02-24 12:04:31 -0500

    commit tree a49e38eb7020c885371af3260d24e5edb6e417d9

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "First Ingredient")
+    @recipe.ingredients.build(name: "Second Ingredient")
   end
 
   def create
+    @recipe = Recipe.create(strong_params)
   end
+
+  private
+
+  def strong_params 
+    params.require(:recipe).permit(:title, ingredients_attributes: [ :name, :quantity ])
+  end
+
 end
